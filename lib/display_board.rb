9863c25a23e8769485317c5c84ef3de408f1f587
Monika Tadross <monikk.tadross@gmail.com>
2019-03-18 04:06:49 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,8 +5,8 @@ board = [" "," "," "," "," "," "," "," "," "]
 
 
 
-def display_board(board_a)
-  puts " #{board_a[0]} | #{board_a[1]} | #{board_a[2]} "
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
